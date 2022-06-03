7664fd963d5c354288415a0c9cf05332b7e15e56
ammarmohmed-1 <ammarmlmero@gmail.com>
2022-06-03 05:12:40 +0200

    aaas

---
+++ script/index.js
@@ -0,0 +1,37 @@
+// style the navbar
+
+var open = document.querySelector(".open");
+var navbar_none = document.querySelector(".navbar_none");
+let i = 0;
+
+open.addEventListener("click", ()=>{
+  navbar_none.classList.toggle("navbar_block");
+  
+});
+
+//  change a background header
+
+var header = document.querySelector(".headerImg_1");
+console.log(header);
+
+var change_img =["headerImg_1","headerImg_2","headerImg_3","headerImg_4"]
+
+setInterval(()=> {
+  header.className = change_img[i]
+  i++
+  if(i == change_img.length){
+    i = 0
+  }
+}, 3000);
+
+
+// Our service
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
